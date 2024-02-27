0c0a938c6c0b3743c91fccc971eb902dc20d2799
stillmvd <asslocker@vk.com>
2024-02-27 20:00:23 +0300

    Добавил первые маршруты

--- app/alloy_airsoft.monolit/importmap.php
+++ app/alloy_airsoft.monolit/importmap.php
@@ -27,4 +27,16 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'react' => [
+        'version' => '18.2.0',
+    ],
+    'react-dom' => [
+        'version' => '18.2.0',
+    ],
+    'scheduler' => [
+        'version' => '0.23.0',
+    ],
+    '@symfony/ux-react' => [
+        'path' => './vendor/symfony/ux-react/assets/dist/loader.js',
+    ],
 ];
