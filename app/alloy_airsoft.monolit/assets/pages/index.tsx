46b3bf2307d4ccae80ee978d2a877cc0381fd4d6
threeh <threeh1@vk.com>
2024-03-08 12:00:59 +0300

    поправил App.tsx

--- app/alloy_airsoft.monolit/assets/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC, ReactElement } from 'react';
-import {AppRouter} from "../app/providers/router";
-
-export const App: FC = (): ReactElement => {
-    return (
-        <AppRouter/>
-    )
-}
