f23ec115b113ef38879c70a45dd2f19aaa394e0a
threeh <threeh1@vk.com>
2024-03-08 13:52:20 +0300

    добавил Throbber

---
+++ app/alloy_airsoft.monolit/assets/pages/eventsPage/ui/EventsPage.tsx
@@ -0,0 +1,13 @@
+import React, { FC, ReactElement } from "react";
+import { Header } from "../../../widgets/Header";
+
+const EventsPage: FC = (): ReactElement => {
+    return (
+        <>
+            <Header />
+            <main></main>
+        </>
+    );
+};
+
+export default EventsPage;
